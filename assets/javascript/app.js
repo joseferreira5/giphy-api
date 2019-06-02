ccc1ebc362e5a079b77dc7efa334e0fe2a648ea3
Jose Ferreira <jferreira590@gmail.com>
2019-06-02 16:14:12 -0400

    added functioning javascript logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,53 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+    console.log("ready!");
 
-var topics = ['baseball','home run','bat flip','strikeout','diving catch'];
-var buttonContainer = $("#buttons");
+    var topics = ['baseball', 'home run', 'bat flip', 'strikeout', 'cat'];
 
-for (let i = 0; i < topics.length; i++) {
-    var topicBtn = $("<button>");
-        topicBtn.addClass("btn ml-3 btn-primary");
-        topicBtn.attr("data-name", topics[i]);
-        topicBtn.text(topics[i]);
-        buttonContainer.append(topicBtn);
-}
+    function renderButton(topic) {
+        var topicBtn = $("<button>");
+        topicBtn.addClass("btn m-2 btn-primary topic-button");
+        topicBtn.attr("data-name", topic);
+        topicBtn.text(topic);
+        $("#buttons").append(topicBtn);
+    }
 
-$("#btn").on("click", function(event) {
+    for (let i = 0; i < topics.length; i++) {
+        renderButton(topics[i]);
+    }
 
-    // Preventing the submit button from trying to submit the form
-    // We're optionally using a form so the user may hit Enter to search instead of clicking the button
-    event.preventDefault();
+    $(document).on("click", ".topic-button", function () {
+        var gifContainer = $("#gif-container");
+        var topic = $(this).attr("data-name");
+        var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=M64M1LXhq0enQ6B35ifqZRKJ9IhNBSAb&limit=10";
 
-    // Here we grab the text from the input box
-    var topic = $("#movie-input").val();
+        gifContainer.empty();
 
-    // Here we construct our URL
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=YOUR_API_KEY&limit=5";
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            var results = response.data;
 
-    // Write code between the dashes below to hit the queryURL with $ajax, then take the response data
-    // and display it in the div with an id of movie-view
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div>");
+                var rating = results[i].rating;
+                var p = $("<p>").text("Rating: " + rating);
+                var gifImage = $("<img>");
 
-    // YOUR CODE GOES IN THESE DASHES. DO NOT MANUALLY EDIT THE HTML ABOVE
+                gifImage.attr("src", results[i].images.fixed_height.url);
+                gifDiv.prepend(p);
+                gifDiv.prepend(gifImage);
 
-    // =================================================================
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function(response){
-                var json = JSON.stringify(response);
-                $("#movie-view").html(json);
-                console.log(response);
-            });
+                gifContainer.prepend(gifDiv);
+            }
+        });
+    });
+
+    $("#add-topic").on("click", function (event) {
+        event.preventDefault();
+        var topicInput = $("#topic-input").val().trim();
+        renderButton(topicInput);
     });
 
 })
